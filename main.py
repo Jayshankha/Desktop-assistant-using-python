447994f3980015531f7f22280c8f75254fdc0bfa
Jayshankha <pjayshankha@gmail.com>
2024-05-20 01:32:24 +0530

    updated

--- main.py
+++ main.py
@@ -18,11 +18,34 @@ engine.setProperty("rate", 150)
 def speak(text):
 
    """ This function takes text and returns voice
-
-   Args:
+       Args:
        text (_type_): string
    
    """
-    engine.say(text)
-    engine.runAndWait()
+engine.say(text)
+engine.runAndWait()
+
+#speech recognization function
+def takeComand():
+    """ this function will recognize voice & return text
+    """
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language= "en-in")
+            print(f"User said: {query}\n")
+
+        except Exception as e:
+            print("say that again please...")
+            return "None"
+        return query
+    
+
+takeComand()
 
